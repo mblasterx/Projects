31cd0db5eab0656aa07aed8a8743de6ebf93bcf5
mblasterx <valentin@mathlinks.ro>
2021-09-19 22:29:43 -0700

    safe password generator

---
+++ safepasswordgenerator.py
@@ -0,0 +1,13 @@
+'''
+Random password generator
+'''
+
+import secrets
+import string
+def main(): 
+    alphabet = string.ascii_letters + string.digits
+    password = ''.join(secrets.choice(alphabet) for i in range(20))
+    print(password)
+
+if __name__ == '__main__':
+    main()
